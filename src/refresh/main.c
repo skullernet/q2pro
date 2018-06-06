ee9154d202f3f02785aaa9faacdda405cafabecc
Andrey Nazarov <skuller@skuller.net>
2018-06-06 19:41:12 +0300

    Enable GLSL rendering backend by default.

--- src/refresh/main.c
+++ src/refresh/main.c
@@ -706,7 +706,7 @@ static void GL_Register(void)
     gl_modulate_entities->changed = gl_modulate_entities_changed;
     gl_doublelight_entities = Cvar_Get("gl_doublelight_entities", "1", 0);
     gl_fontshadow = Cvar_Get("gl_fontshadow", "0", 0);
-    gl_shaders = Cvar_Get("gl_shaders", "0", CVAR_REFRESH);
+    gl_shaders = Cvar_Get("gl_shaders", (gl_config.caps & QGL_CAP_SHADER) ? "1" : "0", CVAR_REFRESH);
 
     // development variables
     gl_znear = Cvar_Get("gl_znear", "2", CVAR_CHEAT);
