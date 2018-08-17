f49707a28219e2bff5eca4fedf3257fbb6242089
Andrey Nazarov <skuller@skuller.net>
2018-08-17 16:29:00 +0300

    Fix edict_t structure private member type.
    Defined as qboolean in quake2-3.21 source, yet the value stored is
    float.

--- src/baseq2/g_local.h
+++ src/baseq2/g_local.h
@@ -1039,7 +1039,7 @@ struct edict_s {
     int         max_health;
     int         gib_health;
     int         deadflag;
-    bool        show_hostile;
+    float       show_hostile;
 
     float       powerarmor_time;
 
