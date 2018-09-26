366ce00c8856c2254da1f9c29da0bda93ce5829e
Unknown <mail@mathyn.com>
2018-09-26 14:14:10 +0200

    Added method to get game details

--- src/client/smilo.c
+++ src/client/smilo.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
 #include "../../inc/common/http.h"
 #include "../../inc/client/smilo.h"
@@ -31,6 +32,54 @@ int CL_Smilo_BetConfirmed() {
             return 1;
         else
             return 0;
+    }
+    else {
+        printf("Failed to do HTTP call...\n");
+        return 0;
+    }
+}
+
+int CL_Smilo_GameInfo(smilo_game_info* output) {
+    printf("Retrieving game info...\n");
+
+    char response[4096];
+    if(HTTP_Get("127.0.0.1", "v1/client/gamedetails", 8090, response, sizeof(response))) {
+        printf("  Agent response: %s\n", response);
+
+        // Line formatting
+        // line 1: input amount
+        // line 2: validated contract ('true' or 'false')
+        // line 3, 4 and 5: first, second and third place rewards
+        int index = 0;
+        for(char* p = strtok(response, "\n"); p != NULL; p = strtok(NULL, "\n")) {
+            printf("Read line: %s\n", p);
+            switch(index) {
+                case(0):
+                    output->input_amount = strtol(p, (char **)NULL, 10);
+                    break;
+                case(1):
+                    output->valid_smart_contract = strcmp(p, "true") ? 1 : 0;
+                    break;
+                case(2):
+                    output->payout_amounts[0] = strtol(p, (char **)NULL, 10);
+                    break;
+                case(3):
+                    output->payout_amounts[1] = strtol(p, (char **)NULL, 10);
+                    break;
+                case(4):
+                    output->payout_amounts[2] = strtol(p, (char **)NULL, 10);
+                    break;
+            }
+
+            if(index == 5)
+                break;
+        }
+
+        // Incomplete response
+        if(index < 5)
+            return 0;
+
+        return 1;
     }
     else {
         printf("Failed to do HTTP call...\n");
