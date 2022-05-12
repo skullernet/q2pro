e39c48b52d40126257690371817420212aa2b32b
Dino <8dino2@gmail.com>
2022-05-11 23:27:05 -0400

    Fixed versioning info problems

--- src/common/common.c
+++ src/common/common.c
@@ -1030,7 +1030,7 @@ void Qcommon_Init(int argc, char **argv)
     Com_AddConfigFile(COM_POSTINIT_CFG, FS_TYPE_REAL);
 
     Com_Printf("====== " PRODUCT " initialized ======\n\n");
-    Com_LPrintf(PRINT_NOTICE, APPLICATION " (" PLATFORM "), " __DATE__ "\n");
+    Com_LPrintf(PRINT_NOTICE, APPLICATION " " PLATFORM ", " __DATE__ "\n");
     Com_Printf("https://aqtiongame.com\n\n");
 
     time(&com_startTime);
