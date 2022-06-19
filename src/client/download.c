76e4b2cadffe79e90f919964d317b08a0ad190b6
Dino <8dino2@gmail.com>
2022-06-19 12:22:41 -0400

    miscompared pointer and int

--- src/client/download.c
+++ src/client/download.c
@@ -881,10 +881,10 @@ void CL_RequestNextDownload(void)
             // Only download jpg textures along with wal if r_override_textures = 1 and
             // r_texture_overrides bitmap is -1 (all), 16 (walls), 32 (skies) or 48 (walls and skies)
             if (r_override_textures->value && 
-                (r_texture_overrides == -1 
-                || r_texture_overrides == 16 
-                || r_texture_overrides == 32 
-                || r_texture_overrides == 48)) 
+                (r_texture_overrides->value == -1 
+                || r_texture_overrides->value == 16 
+                || r_texture_overrides->value == 32 
+                || r_texture_overrides->value == 48)) 
                 {
                 // Some textures do not have high def versions, for those we still default to wal, 
                 // so we download both of them
