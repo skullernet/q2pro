skullernet/q2pro

commit 27df6e1df3ce14992c9d06b2a30ff94bc0641173

Fix invalid pointer comparsion in FS_ExtCmp().

src/common/files.c
@@ -2526,48 +2526,20 @@ bool FS_WildCmp(const char *filter, const char *string)
 
 bool FS_ExtCmp(const char *ext, const char *name)
 {
-    int        c1, c2;
-    const char *e, *n, *l;
+    char *name_ext = COM_FileExtension(name);
+    size_t name_len = strlen(name_ext);
 
-    if (!name[0] || !ext[0]) {
-        return false;
+    while (1) {
+        char *p = Q_strchrnul(ext, ';');
+        size_t len = p - ext;
+        if (name_len == len && !Q_stricmpn(name_ext, ext, len))
+            return true;
+        if (!*p)
+            break;
+        ext = p + 1;
     }
 
-    for (l = name; l[1]; l++)
-        ;
-
-    for (e = ext; e[1]; e++)
-        ;
-
-rescan:
-    n = l;
-    do {
-        c1 = *e--;
-        c2 = *n--;
-
-        if (c1 == ';') {
-            break; // matched
-        }
-
-        if (c1 != c2) {
-            c1 = Q_tolower(c1);
-            c2 = Q_tolower(c2);
-            if (c1 != c2) {
-                while (e > ext) {
-                    c1 = *e--;
-                    if (c1 == ';') {
-                        goto rescan;
-                    }
-                }
-                return false;
-            }
-        }
-        if (n < name) {
-            return false;
-        }
-    } while (e >= ext);
-
-    return true;
+    return false;
 }
 
 static int infocmp(const void *p1, const void *p2)

commit 9a318f050e1a48fbe3ac70805d6193a485e8f445

Fix FS_ExtCmp().
Commit 27df6e1 broke comparing suffixes such as ‘.dm2.gz’, needed for
demo browser menu.

Don't pretend FS_ExtCmp() compares extensions, just compare suffixes
like previous code did.

src/common/files.c
@@ -2514,13 +2514,12 @@ bool FS_WildCmp(const char *filter, const char *string)
 
 bool FS_ExtCmp(const char *ext, const char *name)
 {
-    char *name_ext = COM_FileExtension(name);
-    size_t name_len = strlen(name_ext);
+    size_t name_len = strlen(name);
 
     while (1) {
         char *p = Q_strchrnul(ext, ';');
         size_t len = p - ext;
-        if (name_len == len && !Q_stricmpn(name_ext, ext, len))
+        if (name_len >= len && !Q_stricmpn(name + name_len - len, ext, len))
             return true;
         if (!*p)
             break;

src/common/tests.c
@@ -667,12 +667,12 @@ static const extcmptest_t extcmptests[] = {
     { ".foo;.bar",          "test.FOO",         true  },
     { ".foo;.bar",          "test.baz",         false },
     { ".foo;.BAR;.baz;",    "test.bar",         true  },
-    { ".abc;.foo;.def",     "/path.foo/test",   false },
     { ".abc;.foo;.def",     "",                 false },
     { "",                   "test",             true  },
-    { "",                   "test.foo",         false },
+    { "",                   "test.foo",         true  },
+    { ".foo.bar",           "test.foo.bar",     true  },
     { ".bar;;.baz",         "test",             true  },
-    { ";;;",                "test.foo",         false },
+    { ";;;",                "test.foo",         true  },
 };
 
 static const int numextcmptests = q_countof(extcmptests);