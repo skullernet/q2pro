skullernet/q2pro

commit 02f8ab660c723418bb0e07e95848a46c4ba1c875

Move homedir/baseq2 search path before basedir/game.
Files from game dir should always override baseq2.

src/common/files.c
@@ -3318,6 +3318,11 @@ static void setup_base_paths(void)
     // the GAME bit will be removed once gamedir is set,
     // and will be put back once gamedir is reset to basegame
     add_game_dir(FS_PATH_BASE | FS_PATH_GAME, "%s/"BASEGAME, sys_basedir->string);
+
+    if (sys_homedir->string[0]) {
+        add_game_dir(FS_PATH_BASE | FS_PATH_GAME, "%s/"BASEGAME, sys_homedir->string);
+    }
+
     fs_base_searchpaths = fs_searchpaths;
 }
 
@@ -3332,7 +3337,6 @@ static void setup_game_paths(void)
 
         // home paths override system paths
         if (sys_homedir->string[0]) {
-            add_game_dir(FS_PATH_BASE, "%s/"BASEGAME, sys_homedir->string);
             add_game_dir(FS_PATH_GAME, "%s/%s", sys_homedir->string, fs_game->string);
         }
 
@@ -3343,13 +3347,7 @@ static void setup_game_paths(void)
 
         // this var is set for compatibility with server browsers, etc
         Cvar_FullSet("gamedir", fs_game->string, CVAR_ROM | CVAR_SERVERINFO, FROM_CODE);
-
     } else {
-        if (sys_homedir->string[0]) {
-            add_game_dir(FS_PATH_BASE | FS_PATH_GAME,
-                         "%s/"BASEGAME, sys_homedir->string);
-        }
-
         // add the game bit to base paths
         for (path = fs_base_searchpaths; path; path = path->next) {
             path->mode |= FS_PATH_GAME;

commit 70f476b2116350778f1b414cc2675f1e4b9c0cc9

Fix resetting fs_gamedir after restart.
Broken by 02f8ab66.

src/common/files.c
@@ -3360,6 +3360,18 @@ static void setup_game_paths(void)
     Cvar_FullSet("fs_gamedir", fs_gamedir, CVAR_ROM, FROM_CODE);
 }
 
+static void setup_base_gamedir(void)
+{
+    if (sys_homedir->string[0]) {
+        Q_snprintf(fs_gamedir, sizeof(fs_gamedir), "%s/"BASEGAME, sys_homedir->string);
+    } else {
+        Q_snprintf(fs_gamedir, sizeof(fs_gamedir), "%s/"BASEGAME, sys_basedir->string);
+    }
+#ifdef _WIN32
+    FS_ReplaceSeparators(fs_gamedir, '/');
+#endif
+}
+
 /*
 ================
 FS_Restart
@@ -3378,10 +3390,7 @@ void FS_Restart(bool total)
     } else {
         // just change gamedir
         free_game_paths();
-        Q_snprintf(fs_gamedir, sizeof(fs_gamedir), "%s/"BASEGAME, sys_basedir->string);
-#ifdef _WIN32
-        FS_ReplaceSeparators(fs_gamedir, '/');
-#endif
+        setup_base_gamedir();
     }
 
     setup_game_paths();