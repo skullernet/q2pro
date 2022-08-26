0d534e1902cdd444a6af0cc3b6d2e980e22f0f6a
Andrey Nazarov <skuller@skuller.net>
2022-08-26 18:39:02 +0300

    Fix potential linking failure.

--- src/common/msg.c
+++ src/common/msg.c
@@ -1187,7 +1187,7 @@ int MSG_WriteDeltaPlayerstate_Enhanced(const player_packed_t    *from,
     return eflags;
 }
 
-#if USE_MVD_SERVER || USE_MVD_CLIENT
+#if USE_MVD_SERVER || USE_MVD_CLIENT || USE_CLIENT_GTV
 
 /*
 ==================
@@ -1366,7 +1366,7 @@ void MSG_WriteDeltaPlayerstate_Packet(const player_packed_t *from,
     }
 }
 
-#endif // USE_MVD_SERVER || USE_MVD_CLIENT
+#endif // USE_MVD_SERVER || USE_MVD_CLIENT || USE_CLIENT_GTV
 
 
 /*
