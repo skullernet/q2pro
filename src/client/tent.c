85b05a8d65210ea46c65f39d18ceedee9011a427
Andrey Nazarov <skuller@skuller.net>
2022-09-20 03:13:41 +0300

    Merge identical switch cases.

--- src/client/tent.c
+++ src/client/tent.c
@@ -1055,12 +1055,6 @@ void CL_ParseTEnt(void)
         S_StartSound(te.pos1, 0, 0, cl_sfx_grenexp, 1, ATTN_NORM, 0);
         break;
 
-    case TE_PLASMA_EXPLOSION:
-        CL_PlainExplosion();
-        CL_ExplosionParticles(te.pos1);
-        S_StartSound(te.pos1, 0, 0, cl_sfx_rockexp, 1, ATTN_NORM, 0);
-        break;
-
     case TE_ROCKET_EXPLOSION:
     case TE_ROCKET_EXPLOSION_WATER:
         ex = CL_PlainExplosion();
@@ -1077,6 +1071,7 @@ void CL_ParseTEnt(void)
         break;
 
     case TE_EXPLOSION1:
+    case TE_PLASMA_EXPLOSION:
         CL_PlainExplosion();
         CL_ExplosionParticles(te.pos1);
         S_StartSound(te.pos1, 0, 0, cl_sfx_rockexp, 1, ATTN_NORM, 0);
