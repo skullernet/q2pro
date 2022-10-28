ba25f1dee2ede0647c7a603c919d6e96baf40ba5
adean <8dino2@gmail.com>
2022-10-28 11:02:44 -0400

    More q_shared -> shared

--- inc/shared/shared.h
+++ inc/shared/shared.h
@@ -1178,10 +1178,11 @@ typedef enum {
 
 
 // sound attenuation values
-#define ATTN_NONE               0   // full volume the entire level
+#define ATTN_NONE               0	// full volume the entire level
+#define ATTN_LOUD (loud_guns->value ? 0.4 : 1)
 #define ATTN_NORM               1
 #define ATTN_IDLE               2
-#define ATTN_STATIC             3   // diminish very rapidly with distance
+#define ATTN_STATIC             3	// diminish very rapidly with distance
 
 
 // player_state->stats[] indexes
