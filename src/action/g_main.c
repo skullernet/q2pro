8a4c5134aa32640cbd908bae50918c649b982e41
adean <8dino2@gmail.com>
2022-10-28 10:56:08 -0400

    Removed action version of Com_Printf

--- src/action/g_main.c
+++ src/action/g_main.c
@@ -625,18 +625,6 @@ void Sys_Error (const char *error, ...)
   gi.error("%s", text);
 }
 
-void Com_Printf (const char *msg, ...)
-{
-  va_list argptr;
-  char text[1024];
-
-  va_start (argptr, msg);
-  vsnprintf (text, sizeof(text), msg, argptr);
-  va_end (argptr);
-
-  gi.dprintf("%s", text);
-}
-
 #endif
 
 
