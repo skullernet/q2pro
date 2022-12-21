ae1efc00dfe92cb9b750064bb0ec7566bdc1102b
Andrey Nazarov <skuller@skuller.net>
2022-12-21 20:32:33 +0300

    Allow Netchan_Close() with NULL argument.

--- src/common/net/chan.c
+++ src/common/net/chan.c
@@ -783,6 +783,8 @@ Netchan_Close
 */
 void Netchan_Close(netchan_t *chan)
 {
-    Z_Free(chan->message_buf);
-    Z_Free(chan);
+    if (chan) {
+        Z_Free(chan->message_buf);
+        Z_Free(chan);
+    }
 }
