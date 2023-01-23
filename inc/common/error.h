0707009b2b56748b36707fd228853dfec43353ae
Andrey Nazarov <skuller@skuller.net>
2023-01-23 17:37:42 +0300

    Remove Q_ERR_RUNAWAY_LOOP error code.

--- inc/common/error.h
+++ inc/common/error.h
@@ -47,15 +47,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define Q_ERR_FILE_NOT_REGULAR  _Q_ERR(12)  // Not a regular file
 #define Q_ERR_BAD_RLE_PACKET    _Q_ERR(13)  // Bad run length packet
 #define Q_ERR_STRING_TRUNCATED  _Q_ERR(14)  // String truncation avoided
-#define Q_ERR_RUNAWAY_LOOP      _Q_ERR(15)  // Runaway loop avoided
-#define Q_ERR_INFINITE_LOOP     _Q_ERR(16)  // Infinite loop avoided
-#define Q_ERR_LIBRARY_ERROR     _Q_ERR(17)  // Library error
-#define Q_ERR_OUT_OF_SLOTS      _Q_ERR(18)  // Out of slots
-#define Q_ERR_BAD_ALIGN         _Q_ERR(19)  // Bad lump alignment
-#define Q_ERR_INFLATE_FAILED    _Q_ERR(20)  // Inflate failed
-#define Q_ERR_DEFLATE_FAILED    _Q_ERR(21)  // Deflate failed
-#define Q_ERR_NOT_COHERENT      _Q_ERR(22)  // Coherency check failed
-#define Q_ERR_BAD_COMPRESSION   _Q_ERR(23)  // Bad compression method
+#define Q_ERR_INFINITE_LOOP     _Q_ERR(15)  // Infinite loop avoided
+#define Q_ERR_LIBRARY_ERROR     _Q_ERR(16)  // Library error
+#define Q_ERR_OUT_OF_SLOTS      _Q_ERR(17)  // Out of slots
+#define Q_ERR_BAD_ALIGN         _Q_ERR(18)  // Bad lump alignment
+#define Q_ERR_INFLATE_FAILED    _Q_ERR(19)  // Inflate failed
+#define Q_ERR_DEFLATE_FAILED    _Q_ERR(20)  // Deflate failed
+#define Q_ERR_NOT_COHERENT      _Q_ERR(21)  // Coherency check failed
+#define Q_ERR_BAD_COMPRESSION   _Q_ERR(22)  // Bad compression method
 
 // This macro converts system errno into quake error value.
 #define Q_ERRNO                 Q_ErrorNumber()
