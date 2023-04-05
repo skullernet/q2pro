skullernet/q2pro

commit d29971d56789f3cad6d61b7fbe5949ddadbf3415

Convert some ERR_DROPs to asserts.

src/common/cmodel.c
@@ -1120,8 +1120,7 @@ byte *CM_FatPVS(cm_t *cm, byte *mask, const vec3_t org)
     }
 
     count = CM_BoxLeafs(cm, mins, maxs, leafs, q_countof(leafs), NULL);
-    if (count < 1)
-        Com_Error(ERR_DROP, "CM_FatPVS: leaf count < 1");
+    Q_assert(count > 0 && count < q_countof(leafs));
 
     // convert leafs to clusters
     for (i = 0; i < count; i++) {

src/client/download.c
@@ -60,9 +60,7 @@ int CL_QueueDownload(const char *path, dltype_t type)
     }
 
     len = strlen(path);
-    if (len >= MAX_QPATH) {
-        Com_Error(ERR_DROP, "%s: oversize quake path", __func__);
-    }
+    Q_assert(len < MAX_QPATH);
 
     q = Z_Malloc(sizeof(*q) + len);
     memcpy(q->path, path, len + 1);
@@ -120,12 +118,8 @@ Mark the queue entry as done, decrementing pending count.
 */
 void CL_FinishDownload(dlqueue_t *q)
 {
-    if (q->state == DL_DONE) {
-        Com_Error(ERR_DROP, "%s: already done", __func__);
-    }
-    if (!cls.download.pending) {
-        Com_Error(ERR_DROP, "%s: bad pending count", __func__);
-    }
+    Q_assert(q->state == DL_PENDING || q->state == DL_RUNNING);
+    Q_assert(cls.download.pending > 0);
 
     q->state = DL_DONE;
     cls.download.pending--;
@@ -245,9 +239,7 @@ static bool start_udp_download(dlqueue_t *q)
     int64_t ret;
 
     len = strlen(q->path);
-    if (len >= MAX_QPATH) {
-        Com_Error(ERR_DROP, "%s: oversize quake path", __func__);
-    }
+    Q_assert(len < MAX_QPATH);
 
     // download to a temp name, and only rename
     // to the real name when done, so if interrupted
@@ -894,7 +886,7 @@ void CL_RequestNextDownload(void)
         break;
 
     default:
-        Com_Error(ERR_DROP, "%s: bad precache_check\n", __func__);
+        Q_assert(!"bad precache_check");
     }
 }
 

src/client/effects.c
@@ -90,9 +90,7 @@ void CL_SetLightStyle(int index, const char *s)
 
     ls = &cl_lightstyles[index];
     ls->length = strlen(s);
-    if (ls->length > MAX_QPATH) {
-        Com_Error(ERR_DROP, "%s: oversize style", __func__);
-    }
+    Q_assert(ls->length < MAX_QPATH);
 
     for (i = 0; i < ls->length; i++) {
         ls->map[i] = (float)(s[i] - 'a') / (float)('m' - 'a');

src/client/entities.c
@@ -1248,10 +1248,7 @@ void CL_GetEntitySoundOrigin(int entnum, vec3_t org)
     mmodel_t    *cm;
     vec3_t      mid;
 
-    if (entnum < 0 || entnum >= MAX_EDICTS) {
-        Com_Error(ERR_DROP, "%s: bad entnum: %d", __func__, entnum);
-    }
-
+    Q_assert(entnum >= 0 && entnum < MAX_EDICTS);
     if (!entnum || entnum == listener_entnum) {
         // should this ever happen?
         VectorCopy(listener_origin, org);

src/client/precache.c
@@ -35,12 +35,8 @@ void CL_ParsePlayerSkin(char *name, char *model, char *skin, const char *s)
     size_t len;
     char *t;
 
-    // configstring parsing guarantees that playerskins can never
-    // overflow, but still check the length to be entirely fool-proof
     len = strlen(s);
-    if (len >= MAX_QPATH) {
-        Com_Error(ERR_DROP, "%s: oversize playerskin", __func__);
-    }
+    Q_assert(len < MAX_QPATH);
 
     // isolate the player's name
     t = strchr(s, '\\');

src/client/sound/main.c
@@ -289,10 +289,7 @@ sfx_t *S_SfxForHandle(qhandle_t hSfx)
         return NULL;
     }
 
-    if (hSfx < 1 || hSfx > num_sfx) {
-        Com_Error(ERR_DROP, "S_SfxForHandle: %d out of range", hSfx);
-    }
-
+    Q_assert(hSfx > 0 && hSfx <= num_sfx);
     return &known_sfx[hSfx - 1];
 }
 

src/client/view.c
@@ -138,8 +138,7 @@ void V_AddLightStyle(int style, vec4_t value)
 {
     lightstyle_t    *ls;
 
-    if (style < 0 || style >= MAX_LIGHTSTYLES)
-        Com_Error(ERR_DROP, "Bad light style %i", style);
+    Q_assert(style >= 0 && style < MAX_LIGHTSTYLES);
     ls = &r_lightstyles[style];
 
     //ls->white = r+g+b;

src/common/msg.c
@@ -314,13 +314,8 @@ void MSG_WriteBits(int value, int bits)
     int i;
     size_t bitpos;
 
-    if (bits == 0 || bits < -31 || bits > 32) {
-        Com_Error(ERR_FATAL, "MSG_WriteBits: bad bits: %d", bits);
-    }
-
-    if (msg_write.maxsize - msg_write.cursize < 4) {
-        Com_Error(ERR_FATAL, "MSG_WriteBits: overflow");
-    }
+    Q_assert(!(bits == 0 || bits < -31 || bits > 32));
+    Q_assert(msg_write.maxsize - msg_write.cursize >= 4);
 
     if (bits < 0) {
         bits = -bits;
@@ -455,9 +450,7 @@ void MSG_WriteDir(const vec3_t dir)
 void MSG_PackEntity(entity_packed_t *out, const entity_state_t *in, bool short_angles)
 {
     // allow 0 to accomodate empty baselines
-    if (in->number < 0 || in->number >= MAX_EDICTS)
-        Com_Error(ERR_DROP, "%s: bad number: %d", __func__, in->number);
-
+    Q_assert(in->number >= 0 && in->number < MAX_EDICTS);
     out->number = in->number;
     out->origin[0] = COORD2SHORT(in->origin[0]);
     out->origin[1] = COORD2SHORT(in->origin[1]);
@@ -496,11 +489,8 @@ void MSG_WriteDeltaEntity(const entity_packed_t *from,
     uint32_t    bits, mask;
 
     if (!to) {
-        if (!from)
-            Com_Error(ERR_DROP, "%s: NULL", __func__);
-
-        if (from->number < 1 || from->number >= MAX_EDICTS)
-            Com_Error(ERR_DROP, "%s: bad number: %d", __func__, from->number);
+        Q_assert(from);
+        Q_assert(from->number > 0 && from->number < MAX_EDICTS);
 
         bits = U_REMOVE;
         if (from->number & 0xff00)
@@ -518,8 +508,7 @@ void MSG_WriteDeltaEntity(const entity_packed_t *from,
         return; // remove entity
     }
 
-    if (to->number < 1 || to->number >= MAX_EDICTS)
-        Com_Error(ERR_DROP, "%s: bad number: %d", __func__, to->number);
+    Q_assert(to->number > 0 && to->number < MAX_EDICTS);
 
     if (!from)
         from = &nullEntityState;
@@ -790,8 +779,7 @@ void MSG_WriteDeltaPlayerstate_Default(const player_packed_t *from, const player
     int     pflags;
     int     statbits;
 
-    if (!to)
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
+    Q_assert(to);
 
     if (!from)
         from = &nullPlayerState;
@@ -953,8 +941,7 @@ int MSG_WriteDeltaPlayerstate_Enhanced(const player_packed_t    *from,
     int     pflags, eflags;
     int     statbits;
 
-    if (!to)
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
+    Q_assert(to);
 
     if (!from)
         from = &nullPlayerState;
@@ -1197,6 +1184,7 @@ void MSG_WriteDeltaPlayerstate_Packet(const player_packed_t *from,
     int     pflags;
     int     statbits;
 
+    // this can happen with client GTV
     if (number < 0 || number >= CLIENTNUM_NONE)
         Com_Error(ERR_DROP, "%s: bad number: %d", __func__, number);
 
@@ -1652,9 +1640,7 @@ int MSG_ReadBits(int bits)
     size_t bitpos;
     bool sgn;
 
-    if (bits == 0 || bits < -31 || bits > 32) {
-        Com_Error(ERR_FATAL, "MSG_ReadBits: bad bits: %d", bits);
-    }
+    Q_assert(!(bits == 0 || bits < -31 || bits > 32));
 
     bitpos = msg_read.bitpos;
     if ((bitpos & 7) == 0) {
@@ -1809,13 +1795,8 @@ void MSG_ParseDeltaEntity(const entity_state_t *from,
                           int            bits,
                           msgEsFlags_t   flags)
 {
-    if (!to) {
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
-    }
-
-    if (number < 1 || number >= MAX_EDICTS) {
-        Com_Error(ERR_DROP, "%s: bad entity number: %d", __func__, number);
-    }
+    Q_assert(to);
+    Q_assert(number > 0 && number < MAX_EDICTS);
 
     // set everything to the state we are delta'ing from
     if (!from) {
@@ -1933,9 +1914,7 @@ void MSG_ParseDeltaPlayerstate_Default(const player_state_t *from,
     int         i;
     int         statbits;
 
-    if (!to) {
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
-    }
+    Q_assert(to);
 
     // clear to old value before delta parsing
     if (!from) {
@@ -2048,9 +2027,7 @@ void MSG_ParseDeltaPlayerstate_Enhanced(const player_state_t    *from,
     int         i;
     int         statbits;
 
-    if (!to) {
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
-    }
+    Q_assert(to);
 
     // clear to old value before delta parsing
     if (!from) {
@@ -2183,9 +2160,7 @@ void MSG_ParseDeltaPlayerstate_Packet(const player_state_t *from,
     int         i;
     int         statbits;
 
-    if (!to) {
-        Com_Error(ERR_DROP, "%s: NULL", __func__);
-    }
+    Q_assert(to);
 
     // clear to old value before delta parsing
     if (!from) {

commit 6a70d47abf3422a0a8ed029f455a069a92c27868

Fix broken assertion.
Broken since commit d29971d5.

src/common/cmodel.c
@@ -1124,7 +1124,7 @@ byte *CM_FatPVS(cm_t *cm, byte *mask, const vec3_t org)
     }
 
     count = CM_BoxLeafs(cm, mins, maxs, leafs, q_countof(leafs), NULL);
-    Q_assert(count > 0 && count < q_countof(leafs));
+    Q_assert(count > 0 && count <= q_countof(leafs));
 
     // convert leafs to clusters
     for (i = 0; i < count; i++) {