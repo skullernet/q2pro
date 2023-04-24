skullernet/q2pro

commit f4faabd754193d6e2565ccb2add8dac871f0ac2c

Make ‘if’ command insert raw command args.
Fixes #290.

src/common/cmd.c
@@ -650,6 +650,7 @@ error:
     // scan out branch 1 argument range
     for (j = i; i < Cmd_Argc(); i++) {
         if (!Q_stricmp(Cmd_Argv(i), "else")) {
+            *Cmd_RawArgsFrom(i) = 0;
             break;
         }
     }
@@ -657,12 +658,12 @@ error:
     if (matched) {
         // execute branch 1
         if (i > j) {
-            Cbuf_InsertText(cmd_current, Cmd_ArgsRange(j, i - 1));
+            Cbuf_InsertText(cmd_current, Cmd_RawArgsFrom(j));
         }
     } else {
         // execute branch 2
         if (++i < Cmd_Argc()) {
-            Cbuf_InsertText(cmd_current, Cmd_ArgsFrom(i));
+            Cbuf_InsertText(cmd_current, Cmd_RawArgsFrom(i));
         }
     }
 }

commit 0f35df146c4bbddce38bc46872ade284b357f106

Strip quotes from ‘if’ command arguments.
Fixes backward compatibility issue introduced by commit f4faabd7.
Fixes #297.

inc/shared/shared.h
@@ -468,6 +468,7 @@ int SortStricmp(const void *p1, const void *p2);
 
 size_t COM_strclr(char *s);
 char *COM_StripQuotes(char *s);
+char *COM_Trim(char *s);
 
 // buffer safe operations
 size_t Q_strlcpy(char *dst, const char *src, size_t size);

src/common/cmd.c
@@ -658,12 +658,12 @@ error:
     if (matched) {
         // execute branch 1
         if (i > j) {
-            Cbuf_InsertText(cmd_current, Cmd_RawArgsFrom(j));
+            Cbuf_InsertText(cmd_current, COM_StripQuotes(COM_Trim(Cmd_RawArgsFrom(j))));
         }
     } else {
         // execute branch 2
         if (++i < Cmd_Argc()) {
-            Cbuf_InsertText(cmd_current, Cmd_RawArgsFrom(i));
+            Cbuf_InsertText(cmd_current, COM_StripQuotes(Cmd_RawArgsFrom(i)));
         }
     }
 }

src/shared/shared.c
@@ -348,6 +348,21 @@ char *COM_StripQuotes(char *s)
     return s;
 }
 
+char *COM_Trim(char *s)
+{
+    size_t len;
+
+    while (*s && *s <= ' ')
+        s++;
+
+    len = strlen(s);
+    while (len > 0 && s[len - 1] <= ' ')
+        len--;
+
+    s[len] = 0;
+    return s;
+}
+
 /*
 ============
 va