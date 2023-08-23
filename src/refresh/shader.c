skullernet/q2pro

commit d65ce7c780bca0339b4471746c4846d0aa91eafe

Support N64 texture scroll flags.

src/refresh/shader.c
@@ -30,6 +30,8 @@ enum {
     VERT_ATTR_COLOR
 };
 
+static void upload_u_block(void);
+
 static void write_header(char *buf)
 {
     *buf = 0;
@@ -54,6 +56,7 @@ static void write_block(char *buf)
         GLSL(float u_intensity;)
         GLSL(vec2 w_amp;)
         GLSL(vec2 w_phase;)
+        GLSL(vec2 u_scroll;)
     GLSF("};\n");
 }
 
@@ -74,12 +77,8 @@ static void write_vertex_shader(char *buf, GLbitfield bits)
     }
     GLSF("void main() {\n");
         GLSL(vec2 tc = a_tc;)
-        if (bits & GLS_FLOW_ENABLE) {
-            if (bits & GLS_WARP_ENABLE)
-                GLSL(tc.s -= u_time * 0.5;)
-            else
-                GLSL(tc.s -= 64.0 * fract(u_time * 0.025);)
-        }
+        if (bits & GLS_SCROLL_ENABLE)
+            GLSL(tc += u_time * u_scroll;)
         GLSL(v_tc = tc;)
         if (bits & GLS_LIGHTMAP_ENABLE)
             GLSL(v_lmtc = a_lmtc;)
@@ -255,6 +254,11 @@ static void shader_state_bits(GLbitfield bits)
             qglUseProgram(gl_static.programs[i]);
         else
             gl_static.programs[i] = create_and_use_program(bits);
+    }
+
+    if (diff & GLS_SCROLL_MASK && bits & GLS_SCROLL_ENABLE) {
+        GL_ScrollSpeed(gls.u_block.scroll, bits);
+        upload_u_block();
     }
 }
 

src/refresh/gl.h
@@ -352,16 +352,21 @@ typedef enum {
     GLS_BLEND_MODULATE      = (1 <<  5),
     GLS_ALPHATEST_ENABLE    = (1 <<  6),
     GLS_TEXTURE_REPLACE     = (1 <<  7),
-    GLS_FLOW_ENABLE         = (1 <<  8),
+    GLS_SCROLL_ENABLE       = (1 <<  8),
     GLS_LIGHTMAP_ENABLE     = (1 <<  9),
     GLS_WARP_ENABLE         = (1 << 10),
     GLS_INTENSITY_ENABLE    = (1 << 11),
     GLS_SHADE_SMOOTH        = (1 << 12),
+    GLS_SCROLL_X            = (1 << 13),
+    GLS_SCROLL_Y            = (1 << 14),
+    GLS_SCROLL_FLIP         = (1 << 15),
+    GLS_SCROLL_SLOW         = (1 << 16),
 
     GLS_BLEND_MASK  = GLS_BLEND_BLEND | GLS_BLEND_ADD | GLS_BLEND_MODULATE,
     GLS_COMMON_MASK = GLS_DEPTHMASK_FALSE | GLS_DEPTHTEST_DISABLE | GLS_CULL_DISABLE | GLS_BLEND_MASK,
-    GLS_SHADER_MASK = GLS_ALPHATEST_ENABLE | GLS_TEXTURE_REPLACE | GLS_FLOW_ENABLE |
+    GLS_SHADER_MASK = GLS_ALPHATEST_ENABLE | GLS_TEXTURE_REPLACE | GLS_SCROLL_ENABLE |
         GLS_LIGHTMAP_ENABLE | GLS_WARP_ENABLE | GLS_INTENSITY_ENABLE,
+    GLS_SCROLL_MASK = GLS_SCROLL_ENABLE | GLS_SCROLL_X | GLS_SCROLL_Y | GLS_SCROLL_FLIP | GLS_SCROLL_SLOW,
 } glStateBits_t;
 
 typedef enum {
@@ -388,6 +393,8 @@ typedef struct {
         GLfloat     intensity;
         GLfloat     w_amp[2];
         GLfloat     w_phase[2];
+        GLfloat     scroll[2];
+        GLfloat     pad[2];
     } u_block;
 } glState_t;
 
@@ -479,6 +486,7 @@ static inline void GL_DepthRange(GLfloat n, GLfloat f)
 void GL_ForceTexture(GLuint tmu, GLuint texnum);
 void GL_BindTexture(GLuint tmu, GLuint texnum);
 void GL_CommonStateBits(GLbitfield bits);
+void GL_ScrollSpeed(vec2_t scroll, GLbitfield bits);
 void GL_DrawOutlines(GLsizei count, QGL_INDEX_TYPE *indices);
 void GL_Ortho(GLfloat xmin, GLfloat xmax, GLfloat ymin, GLfloat ymax, GLfloat znear, GLfloat zfar);
 void GL_Frustum(GLfloat fov_x, GLfloat fov_y, GLfloat reflect_x);

src/refresh/legacy.c
@@ -44,24 +44,15 @@ static void legacy_state_bits(GLbitfield bits)
         }
     }
 
-    if (diff & GLS_FLOW_ENABLE) {
+    if (diff & GLS_SCROLL_MASK) {
         GL_ActiveTexture(0);
         qglMatrixMode(GL_TEXTURE);
+        qglLoadIdentity();
 
-        if (bits & GLS_FLOW_ENABLE) {
-            float scaled, scroll;
-
-            if (bits & GLS_WARP_ENABLE) {
-                scaled = glr.fd.time * 0.5f;
-                scroll = -scaled;
-            } else {
-                scaled = glr.fd.time / 40;
-                scroll = -64 * (scaled - (int)scaled);
-            }
-
-            qglTranslatef(scroll, 0, 0);
-        } else {
-            qglLoadIdentity();
+        if (bits & GLS_SCROLL_ENABLE) {
+            vec2_t scroll;
+            GL_ScrollSpeed(scroll, bits);
+            qglTranslatef(scroll[0] * glr.fd.time, scroll[1] * glr.fd.time, 0);
         }
     }
 

src/refresh/state.c
@@ -100,6 +100,27 @@ void GL_CommonStateBits(GLbitfield bits)
     }
 }
 
+void GL_ScrollSpeed(vec2_t scroll, GLbitfield bits)
+{
+    float speed = 1.6f;
+
+    if (bits & (GLS_SCROLL_X | GLS_SCROLL_Y))
+        speed = 0.78125f;
+    else if (bits & GLS_SCROLL_SLOW)
+        speed = 0.5f;
+
+    if (bits & GLS_SCROLL_FLIP)
+        speed = -speed;
+
+    if (bits & GLS_SCROLL_Y) {
+        scroll[0] = 0;
+        scroll[1] = speed;
+    } else {
+        scroll[0] = speed;
+        scroll[1] = 0;
+    }
+}
+
 void GL_Ortho(GLfloat xmin, GLfloat xmax, GLfloat ymin, GLfloat ymax, GLfloat znear, GLfloat zfar)
 {
     GLfloat width, height, depth;

src/refresh/surf.c
@@ -551,7 +551,10 @@ static void build_surface_poly(mface_t *surf, vec_t *vbo)
     }
 
     if (surf->drawflags & SURF_FLOWING) {
-        surf->statebits |= GLS_FLOW_ENABLE;
+        surf->statebits |= GLS_SCROLL_ENABLE | GLS_SCROLL_FLIP;
+        if (surf->drawflags & SURF_WARP) {
+            surf->statebits |= GLS_SCROLL_SLOW;
+        }
     }
 
     // normalize texture coordinates
@@ -561,6 +564,15 @@ static void build_surface_poly(mface_t *surf, vec_t *vbo)
     if (surf->drawflags & SURF_N64_UV) {
         scale[0] *= 0.5f;
         scale[1] *= 0.5f;
+    }
+    if (surf->drawflags & SURF_N64_SCROLL_X) {
+        surf->statebits |= GLS_SCROLL_ENABLE | GLS_SCROLL_X;
+    }
+    if (surf->drawflags & SURF_N64_SCROLL_Y) {
+        surf->statebits |= GLS_SCROLL_ENABLE | GLS_SCROLL_Y;
+    }
+    if (surf->drawflags & SURF_N64_SCROLL_FLIP) {
+        surf->statebits |= GLS_SCROLL_FLIP;
     }
 
     mins[0] = mins[1] = 99999;

src/refresh/tess.c
@@ -455,6 +455,7 @@ void GL_AddSolidFace(mface_t *face)
 
     hash = face->texnum[0] ^ face->texnum[1] ^ face->statebits;
     hash ^= hash >> FACE_HASH_BITS;
+    hash ^= hash >> (FACE_HASH_BITS * 2);
     hash &= FACE_HASH_MASK;
 
     // preserve front-to-back ordering

commit f39df8d360ecf75029aa2c4c20269be183bcaa1a

Add explicit padding to uniform block.
May fix regression on AMD caused by d65ce7c7. See #308.

src/refresh/shader.c
@@ -57,6 +57,7 @@ static void write_block(char *buf)
         GLSL(vec2 w_amp;)
         GLSL(vec2 w_phase;)
         GLSL(vec2 u_scroll;)
+        GLSL(vec2 pad;)
     GLSF("};\n");
 }
 