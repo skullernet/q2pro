89c958f92c1b36f492e85319c630b1164bb988f2
Andrey Nazarov <skuller@skuller.net>
2023-09-16 00:51:29 +0300

    Support TE_POWER_SPLASH.

--- src/client/client.h
+++ src/client/client.h
@@ -859,6 +859,7 @@ void CL_TrapParticles(centity_t *ent, const vec3_t origin);
 void CL_ParticleEffect3(const vec3_t org, const vec3_t dir, int color, int count);
 void CL_ParticleSteamEffect2(cl_sustain_t *self);
 void CL_BerserkSlamParticles(const vec3_t org, const vec3_t dir);
+void CL_PowerSplash(void);
 
 
 //
