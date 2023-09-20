abf25fe50a9fa9ff6424a7bdeff11b3fb39bae81
Andrey Nazarov <skuller@skuller.net>
2023-09-20 16:47:27 +0300

    Support EFX_BARREL_EXPLODING.

--- src/client/client.h
+++ src/client/client.h
@@ -864,6 +864,7 @@ void CL_BerserkSlamParticles(const vec3_t org, const vec3_t dir);
 void CL_PowerSplash(void);
 void CL_TeleporterParticles2(const vec3_t org);
 void CL_HologramParticles(const vec3_t org);
+void CL_BarrelExplodingParticles(const vec3_t org);
 
 
 //
