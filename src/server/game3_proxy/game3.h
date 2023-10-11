2fce8507d3abd4dacd950726bffdbe8c807756a7
Frank Richter <frank.richter@gmail.com>
2023-10-11 12:35:40 +0200

    Add game3_entity_state_t

--- src/server/game3_proxy/game3.h
+++ src/server/game3_proxy/game3.h
@@ -21,7 +21,26 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "shared/list.h"
 
-typedef entity_state_t game3_entity_state_t;
+typedef struct game3_entity_state_s {
+    int     number;         // edict index
+
+    vec3_t  origin;
+    vec3_t  angles;
+    vec3_t  old_origin;     // for lerping
+    int     modelindex;
+    int     modelindex2, modelindex3, modelindex4;  // weapons, CTF flags, etc
+    int     frame;
+    int     skinnum;
+    unsigned int        effects;        // PGM - we're filling it, so it needs to be unsigned
+    int     renderfx;
+    int     solid;          // for client side prediction, 8*(bits 0-4) is x/y radius
+                            // 8*(bits 5-9) is z down distance, 8(bits10-15) is z up
+                            // gi.linkentity sets this properly
+    int     sound;          // for looping sounds, to guarantee shutoff
+    int     event;          // impulse events -- muzzle flashes, footsteps, etc
+                            // events only go out for a single frame, they
+                            // are automatically cleared each frame
+} game3_entity_state_t;
 
 typedef struct game3_edict_s game3_edict_t;
 
