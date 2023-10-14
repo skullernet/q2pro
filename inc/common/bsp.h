c3c3caf2658cd66d2ec83a86b21afad6c007a400
Jonathan <jonno.5000@gmail.com>
2023-10-14 03:35:16 -0400

    fixed-width habits die hard

--- inc/common/bsp.h
+++ inc/common/bsp.h
@@ -310,9 +310,9 @@ typedef struct {
     float       fraction;
 } lightpoint_t;
 
-void BSP_LightPoint(lightpoint_t *point, const vec3_t start, const vec3_t end, mnode_t *headnode, int32_t nolm_mask);
+void BSP_LightPoint(lightpoint_t *point, const vec3_t start, const vec3_t end, mnode_t *headnode, int nolm_mask);
 void BSP_TransformedLightPoint(lightpoint_t *point, const vec3_t start, const vec3_t end,
-                               mnode_t *headnode, int32_t nolm_mask, const vec3_t origin, const vec3_t angles);
+                               mnode_t *headnode, int nolm_mask, const vec3_t origin, const vec3_t angles);
 
 lightgrid_sample_t *BSP_LookupLightgrid(lightgrid_t *grid, int32_t point[3]);
 #endif
