59215dec80ac2dd70c2e18141c49b71eaa74c378
acyc070 <112773735+acyc070@users.noreply.github.com>
2023-12-09 01:40:25 +0400

    Update md2.h

--- inc/format/md2.h
+++ inc/format/md2.h
@@ -29,13 +29,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define MD2_IDENT       MakeLittleLong('I','D','P','2')
 #define MD2_VERSION     8
 
-#define MD2_MAX_TRIANGLES   4096
-#define MD2_MAX_VERTS       2048
-#define MD2_MAX_FRAMES      1024
-#define MD2_MAX_SKINS       32
-#define MD2_MAX_SKINNAME    64
-#define MD2_MAX_SKINWIDTH   640
-#define MD2_MAX_SKINHEIGHT  480
+#define MD2_MAX_TRIANGLES   9999
+#define MD2_MAX_VERTS       9999
+#define MD2_MAX_FRAMES      9999
+#define MD2_MAX_SKINS       9999
+#define MD2_MAX_SKINNAME    9999
+#define MD2_MAX_SKINWIDTH   9999
+#define MD2_MAX_SKINHEIGHT  9999
 
 typedef struct {
     int16_t    s;
