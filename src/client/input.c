skullernet/q2pro

commit 84cfacb2d4478b42c1f0a4827c912a098e4de14f

Rewrite bits r/w to use bits cache.
Avoids annoying requirement to maintain bitpos in SZ_*() functions.

src/client/input.c
@@ -979,6 +979,8 @@ static void CL_SendBatchedCmd(void)
     cl.lastTransmitCmdNumber = cl.cmdNumber;
     cl.lastTransmitCmdNumberReal = cl.cmdNumber;
 
+    MSG_BeginWriting();
+
     // begin a client move command
     patch = SZ_GetSpace(&msg_write, 1);
 
@@ -1028,6 +1030,8 @@ static void CL_SendBatchedCmd(void)
             oldcmd = cmd;
         }
     }
+
+    MSG_FlushBits();
 
     P_FRAMES++;
 

inc/common/msg.h
@@ -100,6 +100,7 @@ void    MSG_WriteString(const char *s);
 void    MSG_WritePos(const vec3_t pos);
 void    MSG_WriteAngle(float f);
 #if USE_CLIENT
+void    MSG_FlushBits(void);
 void    MSG_WriteBits(int value, int bits);
 int     MSG_WriteDeltaUsercmd(const usercmd_t *from, const usercmd_t *cmd, int version);
 int     MSG_WriteDeltaUsercmd_Enhanced(const usercmd_t *from, const usercmd_t *cmd);

inc/common/sizebuf.h
@@ -27,7 +27,8 @@ typedef struct {
     size_t      maxsize;
     size_t      cursize;
     size_t      readcount;
-    size_t      bitpos;
+    uint32_t    bits_buf;
+    uint32_t    bits_left;
     const char  *tag;           // for debugging
 } sizebuf_t;
 

src/common/msg.c
@@ -78,7 +78,8 @@ MSG_BeginWriting
 void MSG_BeginWriting(void)
 {
     msg_write.cursize = 0;
-    msg_write.bitpos = 0;
+    msg_write.bits_buf = 0;
+    msg_write.bits_left = 32;
     msg_write.overflowed = false;
 }
 
@@ -308,42 +309,46 @@ MSG_WriteBits
 */
 void MSG_WriteBits(int value, int bits)
 {
-    int i;
-    size_t bitpos;
-
-    Q_assert(!(bits == 0 || bits < -31 || bits > 32));
-    Q_assert(msg_write.maxsize - msg_write.cursize >= 4);
+    Q_assert(!(bits == 0 || bits < -31 || bits > 31));
 
     if (bits < 0) {
         bits = -bits;
     }
 
-    bitpos = msg_write.bitpos;
-    if ((bitpos & 7) == 0) {
-        // optimized case
-        switch (bits) {
-        case 8:
-            MSG_WriteByte(value);
-            return;
-        case 16:
-            MSG_WriteShort(value);
-            return;
-        case 32:
-            MSG_WriteLong(value);
-            return;
-        default:
-            break;
-        }
+    uint32_t bits_buf  = msg_write.bits_buf;
+    uint32_t bits_left = msg_write.bits_left;
+    uint32_t v = value & ((1U << bits) - 1);
+
+    bits_buf |= v << (32 - bits_left);
+    if (bits >= bits_left) {
+        MSG_WriteLong(bits_buf);
+        bits_buf   = v >> bits_left;
+        bits_left += 32;
     }
-    for (i = 0; i < bits; i++, bitpos++) {
-        if ((bitpos & 7) == 0) {
-            msg_write.data[bitpos >> 3] = 0;
-        }
-        msg_write.data[bitpos >> 3] |= (value & 1) << (bitpos & 7);
-        value >>= 1;
+    bits_left -= bits;
+
+    msg_write.bits_buf  = bits_buf;
+    msg_write.bits_left = bits_left;
+}
+
+/*
+=============
+MSG_FlushBits
+=============
+*/
+void MSG_FlushBits(void)
+{
+    uint32_t bits_buf  = msg_write.bits_buf;
+    uint32_t bits_left = msg_write.bits_left;
+
+    while (bits_left < 32) {
+        MSG_WriteByte(bits_buf & 255);
+        bits_buf >>= 8;
+        bits_left += 8;
     }
-    msg_write.bitpos = bitpos;
-    msg_write.cursize = (bitpos + 7) >> 3;
+
+    msg_write.bits_buf  = 0;
+    msg_write.bits_left = 32;
 }
 
 /*
@@ -1358,7 +1363,8 @@ void MSG_WriteDeltaPlayerstate_Packet(const player_packed_t *from,
 void MSG_BeginReading(void)
 {
     msg_read.readcount = 0;
-    msg_read.bitpos = 0;
+    msg_read.bits_buf  = 0;
+    msg_read.bits_left = 0;
 }
 
 byte *MSG_ReadData(size_t len)
@@ -1639,51 +1645,30 @@ void MSG_ReadDeltaUsercmd_Hacked(const usercmd_t *from, usercmd_t *to)
 
 int MSG_ReadBits(int bits)
 {
-    int i, value;
-    size_t bitpos;
-    bool sgn;
+    bool sgn = false;
 
-    Q_assert(!(bits == 0 || bits < -31 || bits > 32));
+    Q_assert(!(bits == 0 || bits < -25 || bits > 25));
 
-    bitpos = msg_read.bitpos;
-    if ((bitpos & 7) == 0) {
-        // optimized case
-        switch (bits) {
-        case -8:
-            value = MSG_ReadChar();
-            return value;
-        case 8:
-            value = MSG_ReadByte();
-            return value;
-        case -16:
-            value = MSG_ReadShort();
-            return value;
-        case 32:
-            value = MSG_ReadLong();
-            return value;
-        default:
-            break;
-        }
-    }
-
-    sgn = false;
     if (bits < 0) {
         bits = -bits;
         sgn = true;
     }
 
-    value = 0;
-    for (i = 0; i < bits; i++, bitpos++) {
-        unsigned get = (msg_read.data[bitpos >> 3] >> (bitpos & 7)) & 1;
-        value |= get << i;
+    uint32_t bits_buf  = msg_read.bits_buf;
+    uint32_t bits_left = msg_read.bits_left;
+
+    while (bits > bits_left) {
+        bits_buf  |= (uint32_t)MSG_ReadByte() << bits_left;
+        bits_left += 8;
     }
-    msg_read.bitpos = bitpos;
-    msg_read.readcount = (bitpos + 7) >> 3;
+
+    uint32_t value = bits_buf & ((1U << bits) - 1);
+
+    msg_read.bits_buf  = bits_buf >> bits;
+    msg_read.bits_left = bits_left - bits;
 
     if (sgn) {
-        if (value & (1 << (bits - 1))) {
-            value |= -1 ^ ((1U << bits) - 1);
-        }
+        return (int32_t)(value << (32 - bits)) >> (32 - bits);
     }
 
     return value;

src/common/sizebuf.c
@@ -43,7 +43,6 @@ void SZ_Clear(sizebuf_t *buf)
 {
     buf->cursize = 0;
     buf->readcount = 0;
-    buf->bitpos = 0;
     buf->overflowed = false;
 }
 
@@ -77,7 +76,6 @@ void *SZ_GetSpace(sizebuf_t *buf, size_t len)
 
     data = buf->data + buf->cursize;
     buf->cursize += len;
-    buf->bitpos = buf->cursize << 3;
     return data;
 }
 
@@ -139,13 +137,11 @@ void *SZ_ReadData(sizebuf_t *buf, size_t len)
             Com_Error(ERR_DROP, "%s: read past end of message", __func__);
         }
         buf->readcount = buf->cursize + 1;
-        buf->bitpos = buf->readcount << 3;
         return NULL;
     }
 
     data = buf->data + buf->readcount;
     buf->readcount += len;
-    buf->bitpos = buf->readcount << 3;
     return data;
 }
 

src/server/user.c
@@ -1171,7 +1171,7 @@ static void SV_NewClientExecuteMove(int c)
     lastcmd = NULL;
     for (i = 0; i <= numDups; i++) {
         numCmds[i] = MSG_ReadBits(5);
-        if (numCmds[i] == -1) {
+        if (msg_read.readcount > msg_read.cursize) {
             SV_DropClient(sv_client, "read past end of message");
             return;
         }

commit a98de5682222d36f2bfc34f1031cfe927e9243b8

Clear cached bits if MAX_PACKET_USERCMDS is exceeded.
Broken since commit 84cfacb2.

src/client/input.c
@@ -1014,7 +1014,7 @@ static void CL_SendBatchedCmd(void)
         numCmds = history->cmdNumber - oldest->cmdNumber;
         if (numCmds >= MAX_PACKET_USERCMDS) {
             Com_WPrintf("%s: MAX_PACKET_USERCMDS exceeded\n", __func__);
-            SZ_Clear(&msg_write);
+            MSG_BeginWriting();
             break;
         }
         totalCmds += numCmds;