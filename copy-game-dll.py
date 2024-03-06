f1d61867f7eb61ce884e322e20167a40ccc560c5
Frank Richter <frank.richter@gmail.com>
2024-03-06 01:34:01 +0100

    Simplify game DLL copying & fix installation
    (cherry picked from commit fd4134faa3c5b229c12423da25d898b1701fc35b)

--- copy-game-dll.py
+++ copy-game-dll.py
@@ -9,16 +9,15 @@ import shutil
 import stat
 
 args = argparse.ArgumentParser()
-args.add_argument('stamp_file', help="timestamp file")
+args.add_argument('output_name', help="output file name")
 args.add_argument('game_dll', help="game shared library path")
-args.add_argument('output_dir', help="output directory")
 arg_values = args.parse_args()
 
 game_dll_path = pathlib.Path(arg_values.game_dll)
-output_path = pathlib.Path(arg_values.output_dir)
+dest_path = pathlib.Path(arg_values.output_name)
+output_path = dest_path.parent
 
 # Copy game DLL to output directory
-dest_path = output_path / game_dll_path.name
 shutil.copy2(game_dll_path, dest_path)
 
 # Additional file extensions which are copied over as well, if existing.
@@ -29,9 +28,3 @@ for ext in ADDITIONAL_EXTS:
     if candidate.exists():
         dest_path = output_path / candidate.name
         shutil.copy2(candidate, dest_path)
-
-# Finally, copy metadata to stamp file.
-open(arg_values.stamp_file, "w") # create, if not yet existing
-shutil.copystat(arg_values.game_dll, arg_values.stamp_file)
-# Reset executable bits
-os.chmod(arg_values.stamp_file, os.stat(arg_values.stamp_file).st_mode & ~(stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH))
