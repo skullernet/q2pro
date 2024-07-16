skullernet/q2pro

commit 65061f5bb32c04db585fa0a3c639f7b10d1213a2

Hold player for 200 ms after initial spawn.
Since loading the level can be very fast, give user some time to release
keys from previous action, like it is done after regular respawns and
teleports. Closes #312.

src/game/p_client.c
@@ -1233,6 +1233,10 @@ void ClientBeginDeathmatch(edict_t *ent)
         gi.WriteShort(ent - g_edicts);
         gi.WriteByte(MZ_LOGIN);
         gi.multicast(ent->s.origin, MULTICAST_PVS);
+
+        // hold in place briefly
+        ent->client->ps.pmove.pm_flags = PMF_TIME_TELEPORT;
+        ent->client->ps.pmove.pm_time = 200 >> 3;
     }
 
     gi.bprintf(PRINT_HIGH, "%s entered the game\n", ent->client->pers.netname);
@@ -1277,6 +1281,10 @@ void ClientBegin(edict_t *ent)
         ent->classname = "player";
         InitClientResp(ent->client);
         PutClientInServer(ent);
+
+        // hold in place briefly
+        ent->client->ps.pmove.pm_flags = PMF_TIME_TELEPORT;
+        ent->client->ps.pmove.pm_time = 200 >> 3;
     }
 
     if (level.intermission_framenum) {

commit cb4f6f8daa19dd8413ff2f0da7b79f4097e9e86e

Fix broken viewangles with ‘spectator 1’.
Broken by commit 65061f5b.

src/game/p_client.c
@@ -1227,7 +1227,7 @@ void ClientBeginDeathmatch(edict_t *ent)
 
     if (level.intermission_framenum) {
         MoveClientToIntermission(ent);
-    } else {
+    } else if (!ent->client->pers.spectator) {
         // send effect
         gi.WriteByte(svc_muzzleflash);
         gi.WriteShort(ent - g_edicts);