skullernet/q2pro

commit 704d747e403302f6275f42f693020b4af0dcc4b2

Simplify GL_TextureAnimation().

src/refresh/tess.c
@@ -506,14 +506,9 @@ static int GL_CopyVerts(const mface_t *surf)
 
 static const image_t *GL_TextureAnimation(const mtexinfo_t *tex)
 {
-    if (q_unlikely(tex->next)) {
-        unsigned c = glr.ent->frame % tex->numframes;
-
-        while (c) {
+    if (q_unlikely(tex->next))
+        for (int i = 0; i < glr.ent->frame % tex->numframes; i++)
             tex = tex->next;
-            c--;
-        }
-    }
 
     return tex->image;
 }

commit 8fea803fd0ee56036a5ea0452768e1ad339f3338

Fix broken GL_TextureAnimation().
Broken by commit 704d747e. It still worked, but the code was wrong.

src/refresh/tess.c
@@ -592,9 +592,11 @@ static int GL_CopyVerts(const mface_t *surf)
 
 static const image_t *GL_TextureAnimation(const mtexinfo_t *tex)
 {
-    if (q_unlikely(tex->next))
-        for (int i = 0; i < glr.ent->frame % tex->numframes; i++)
+    if (q_unlikely(tex->next)) {
+        int c = glr.ent->frame % tex->numframes;
+        while (c--)
             tex = tex->next;
+    }
 
     return tex->image;
 }