skullernet/q2pro

commit 47656f88fed5cac9b395992cc0d9a63accd593d8

Add support for cubemap skies.
Support loading cubemap textures for SURF_SKY faces. This allows
multiple skybox textures per map. Also rewrite N64 skies to not use
skybox drawing code at all.

Also fully support skies on bmodels, but only enable them in
DECOUPLED_LM maps.

inc/refresh/refresh.h
@@ -155,6 +155,7 @@ typedef enum {
     IF_NEAREST          = BIT(7),   // don't bilerp
     IF_OPAQUE           = BIT(8),   // known to be opaque
     IF_DEFAULT_FLARE    = BIT(9),   // default flare hack
+    IF_CUBEMAP          = BIT(10),  // cubemap (or part of it)
 
     // not stored in image
     IF_OPTIONAL         = BIT(16),  // don't warn if not found

src/refresh/surf.c
@@ -573,6 +573,13 @@ static glStateBits_t statebits_for_surface(const mface_t *surf)
 {
     glStateBits_t statebits = GLS_DEFAULT;
 
+    if (surf->drawflags & SURF_SKY) {
+        if (Q_stricmpn(surf->texinfo->name, CONST_STR_LEN("n64/env/sky")) == 0)
+            return GLS_TEXTURE_REPLACE | GLS_CLASSIC_SKY;
+        else
+            return GLS_TEXTURE_REPLACE | GLS_DEFAULT_SKY;
+    }
+
     if (gl_static.use_shaders) {
         // no inverse intensity
         if (!(surf->drawflags & SURF_TRANS_MASK))
@@ -887,7 +894,9 @@ static void upload_world_surfaces(void)
     currvert = 0;
     lastvert = 0;
     for (i = 0, surf = bsp->faces; i < bsp->numfaces; i++, surf++) {
-        if (surf->drawflags & (SURF_SKY | SURF_NODRAW))
+        if (surf->drawflags & SURF_SKY && !gl_static.use_cubemaps)
+            continue;
+        if (surf->drawflags & SURF_NODRAW)
             continue;
 
         Q_assert(surf->numsurfedges >= 3 && surf->numsurfedges <= TESS_MAX_VERTICES);
@@ -974,7 +983,6 @@ void GL_FreeWorld(void)
         return;
 
     BSP_Free(gl_static.world.cache);
-    gl_static.classic_sky = NULL;
 
     if (gl_static.world.vertices)
         Z_Free(gl_static.world.vertices);
@@ -1032,28 +1040,27 @@ void GL_LoadWorld(const char *name)
     GL_InitQueries();
 
     gl_static.world.cache = bsp;
-    gl_static.classic_sky = NULL;
 
     // calculate world size for far clip plane and sky box
     set_world_size(bsp->nodes);
 
     // register all texinfo
     for (i = 0, info = bsp->texinfo; i < bsp->numtexinfo; i++, info++) {
-        if (gl_static.use_shaders && info->c.flags & SURF_SKY &&
-            Q_stricmpn(info->name, CONST_STR_LEN("n64/env/sky")) == 0) {
-            if (gl_static.classic_sky) {
-                info->image = gl_static.classic_sky;
-            } else {
+        if (info->c.flags & SURF_SKY) {
+            if (!gl_static.use_cubemaps) {
+                info->image = R_NOTEXTURE;
+            } else if (Q_stricmpn(info->name, CONST_STR_LEN("n64/env/sky")) == 0) {
                 Q_concat(buffer, sizeof(buffer), "textures/", info->name, ".tga");
-                FS_NormalizePath(buffer);
                 info->image = IMG_Find(buffer, IT_SKY, IF_REPEAT | IF_CLASSIC_SKY);
-                if (info->image != R_NOTEXTURE)
-                    gl_static.classic_sky = info->image;
+            } else if (Q_stricmpn(info->name, CONST_STR_LEN("sky/")) == 0) {
+                Q_concat(buffer, sizeof(buffer), info->name, ".tga");
+                info->image = IMG_Find(buffer, IT_SKY, IF_CUBEMAP);
+            } else {
+                info->image = R_SKYTEXTURE;
             }
         } else {
             imageflags_t flags = (info->c.flags & SURF_WARP) ? IF_TURBULENT : IF_NONE;
             Q_concat(buffer, sizeof(buffer), "textures/", info->name, ".wal");
-            FS_NormalizePath(buffer);
             info->image = IMG_Find(buffer, IT_WALL, flags);
         }
     }
@@ -1064,8 +1071,16 @@ void GL_LoadWorld(const char *name)
         // hack surface flags into drawflags for faster access
         surf->drawflags |= surf->texinfo->c.flags & ~DSURF_PLANEBACK;
 
+        // clear statebits from previous load
+        surf->statebits = GLS_DEFAULT;
+
         // don't count sky surfaces
-        if (surf->drawflags & (SURF_SKY | SURF_NODRAW))
+        if (surf->drawflags & SURF_SKY) {
+            if (!gl_static.use_cubemaps)
+                continue;
+            surf->drawflags &= ~SURF_NODRAW;
+        }
+        if (surf->drawflags & SURF_NODRAW)
             continue;
         if (surf->drawflags & SURF_N64_UV)
             n64surfs++;
@@ -1082,12 +1097,15 @@ void GL_LoadWorld(const char *name)
     }
 
     gl_static.nolm_mask = SURF_NOLM_MASK_DEFAULT;
+    gl_static.use_bmodel_skies = false;
 
     // only supported in DECOUPLED_LM maps because vanilla maps have broken
     // lightofs for liquids/alphas. legacy renderer doesn't support lightmapped
     // liquids too.
-    if ((bsp->lm_decoupled || n64surfs > 100 || gl_static.classic_sky) && gl_static.use_shaders)
+    if ((bsp->lm_decoupled || n64surfs > 100) && gl_static.use_shaders) {
         gl_static.nolm_mask = SURF_NOLM_MASK_REMASTER;
+        gl_static.use_bmodel_skies = gl_static.use_cubemaps;
+    }
 
     glr.fd.lightstyles = &(lightstyle_t){ 1 };
 

doc/client.asciidoc
@@ -674,11 +674,14 @@ gl_downsample_skins::
     Default value is 1 (downsampling enabled).
 
 gl_drawsky::
-    Enables skybox texturing and N64 skies. N64 skies only work if
-    ‘gl_shaders’ is enabled. Default value is 1.
-      - 0 — draw solid black sky
-      - 1 — draw normal sky, or N64 sky if present
-      - 2 — draw normal sky, ignore N64 sky
+    Enables skybox texturing. 0 means to draw sky in solid black color.
+    Default value is 1 (enabled).
+
+gl_cubemaps::
+    Enables use of cubemaps for skybox drawing. Cubemaps allow multiple skybox
+    textures per map, and help to avoid sky rendering bugs. Also enables N64
+    skies (although these aren't technically cubemaps). Only effective if
+    ‘gl_shaders’ is enabled. Default value is 1 (enabled).
 
 gl_waterwarp::
     Enable screen warping effect when underwater. Only effective when using

src/refresh/gl.h
@@ -50,7 +50,7 @@ typedef GLuint glIndex_t;
 #define TAB_SIN(x)  gl_static.sintab[(x) & 255]
 #define TAB_COS(x)  gl_static.sintab[((x) + 64) & 255]
 
-#define NUM_AUTO_TEXTURES   7
+#define NUM_AUTO_TEXTURES   9
 
 typedef struct {
     GLuint query;
@@ -63,6 +63,8 @@ typedef struct {
 typedef struct {
     bool            registering;
     bool            use_shaders;
+    bool            use_cubemaps;
+    bool            use_bmodel_skies;
     struct {
         bsp_t       *cache;
         vec_t       *vertices;
@@ -90,7 +92,6 @@ typedef struct {
     byte            lightstylemap[MAX_LIGHTSTYLES];
     hash_map_t      *queries;
     hash_map_t      *programs;
-    image_t         *classic_sky;
 } glStatic_t;
 
 typedef struct {
@@ -111,6 +112,7 @@ typedef struct {
     float           entscale;
     vec3_t          entaxis[3];
     GLfloat         entmatrix[16];
+    GLfloat         skymatrix[2][16];
     lightpoint_t    lightpoint;
     int             num_beams;
     int             num_flares;
@@ -245,7 +247,7 @@ bool GL_AllocBlock(int width, int height, uint16_t *inuse,
 void GL_MultMatrix(GLfloat *restrict out, const GLfloat *restrict a, const GLfloat *restrict b);
 void GL_SetEntityAxis(void);
 void GL_RotationMatrix(GLfloat *matrix);
-void GL_RotateForEntity(void);
+void GL_RotateForEntity(bool skies);
 
 void GL_ClearErrors(void);
 bool GL_ShowErrors(const char *func);
@@ -471,19 +473,21 @@ typedef enum {
     GLS_INTENSITY_ENABLE    = BIT(11),
     GLS_GLOWMAP_ENABLE      = BIT(12),
     GLS_CLASSIC_SKY         = BIT(13),
-    GLS_DEFAULT_FLARE       = BIT(14),
+    GLS_DEFAULT_SKY         = BIT(14),
+    GLS_DEFAULT_FLARE       = BIT(15),
 
-    GLS_SHADE_SMOOTH        = BIT(15),
-    GLS_SCROLL_X            = BIT(16),
-    GLS_SCROLL_Y            = BIT(17),
-    GLS_SCROLL_FLIP         = BIT(18),
-    GLS_SCROLL_SLOW         = BIT(19),
+    GLS_SHADE_SMOOTH        = BIT(16),
+    GLS_SCROLL_X            = BIT(17),
+    GLS_SCROLL_Y            = BIT(18),
+    GLS_SCROLL_FLIP         = BIT(19),
+    GLS_SCROLL_SLOW         = BIT(20),
 
     GLS_BLEND_MASK  = GLS_BLEND_BLEND | GLS_BLEND_ADD | GLS_BLEND_MODULATE,
     GLS_COMMON_MASK = GLS_DEPTHMASK_FALSE | GLS_DEPTHTEST_DISABLE | GLS_CULL_DISABLE | GLS_BLEND_MASK,
+    GLS_SKY_MASK    = GLS_CLASSIC_SKY | GLS_DEFAULT_SKY,
     GLS_SHADER_MASK = GLS_ALPHATEST_ENABLE | GLS_TEXTURE_REPLACE | GLS_SCROLL_ENABLE |
         GLS_LIGHTMAP_ENABLE | GLS_WARP_ENABLE | GLS_INTENSITY_ENABLE | GLS_GLOWMAP_ENABLE |
-        GLS_CLASSIC_SKY | GLS_DEFAULT_FLARE,
+        GLS_SKY_MASK | GLS_DEFAULT_FLARE,
     GLS_SCROLL_MASK = GLS_SCROLL_ENABLE | GLS_SCROLL_X | GLS_SCROLL_Y | GLS_SCROLL_FLIP | GLS_SCROLL_SLOW,
 } glStateBits_t;
 
@@ -529,6 +533,7 @@ typedef enum {
 
 typedef struct {
     GLfloat     mvp[16];
+    GLfloat     msky[2][16];
     GLfloat     time;
     GLfloat     modulate;
     GLfloat     add;
@@ -538,14 +543,13 @@ typedef struct {
     GLfloat     w_amp[2];
     GLfloat     w_phase[2];
     GLfloat     scroll[2];
-    GLfloat     vieworg[3];
-    GLfloat     pad_2;
 } glUniformBlock_t;
 
 typedef struct {
     glTmu_t             client_tmu;
     glTmu_t             server_tmu;
     GLuint              texnums[MAX_TMUS];
+    GLuint              texnumcube;
     glStateBits_t       state_bits;
     glArrayBits_t       array_bits;
     GLuint              currentbuffer[2];
@@ -675,6 +679,8 @@ typedef enum {
 
 void GL_ForceTexture(glTmu_t tmu, GLuint texnum);
 void GL_BindTexture(glTmu_t tmu, GLuint texnum);
+void GL_ForceCubemap(GLuint texnum);
+void GL_BindCubemap(GLuint texnum);
 void GL_CommonStateBits(glStateBits_t bits);
 void GL_ScrollPos(vec2_t scroll, glStateBits_t bits);
 void GL_DrawOutlines(GLsizei count, const glIndex_t *indices, bool indexed);
@@ -715,13 +721,15 @@ void GL_Blend(void);
  */
 
 // auto textures
-#define TEXNUM_DEFAULT  gl_static.texnums[0]
-#define TEXNUM_SCRAP    gl_static.texnums[1]
-#define TEXNUM_PARTICLE gl_static.texnums[2]
-#define TEXNUM_BEAM     gl_static.texnums[3]
-#define TEXNUM_WHITE    gl_static.texnums[4]
-#define TEXNUM_BLACK    gl_static.texnums[5]
-#define TEXNUM_RAW      gl_static.texnums[6]
+#define TEXNUM_DEFAULT          gl_static.texnums[0]
+#define TEXNUM_SCRAP            gl_static.texnums[1]
+#define TEXNUM_PARTICLE         gl_static.texnums[2]
+#define TEXNUM_BEAM             gl_static.texnums[3]
+#define TEXNUM_WHITE            gl_static.texnums[4]
+#define TEXNUM_BLACK            gl_static.texnums[5]
+#define TEXNUM_RAW              gl_static.texnums[6]
+#define TEXNUM_CUBEMAP_DEFAULT  gl_static.texnums[7]
+#define TEXNUM_CUBEMAP_BLACK    gl_static.texnums[8]
 
 void Scrap_Upload(void);
 
@@ -731,8 +739,6 @@ void GL_ShutdownImages(void);
 bool GL_InitWarpTexture(void);
 
 extern cvar_t *gl_intensity;
-
-extern image_t shell_texture;
 
 /*
  * gl_tess.c
@@ -788,6 +794,7 @@ void GL_LightPoint(const vec3_t origin, vec3_t color);
 void R_AddSkySurface(const mface_t *surf);
 void R_ClearSkyBox(void);
 void R_DrawSkyBox(void);
+void R_RotateForSky(void);
 void R_SetSky(const char *name, float rotate, bool autorotate, const vec3_t axis);
 
 /*

src/refresh/images.c
@@ -1557,7 +1557,7 @@ static void IMG_List_f(void)
     Com_Printf("------------------\n");
     texels = count = 0;
 
-    for (i = 1, image = r_images + 1; i < r_numImages; i++, image++) {
+    for (i = R_NUM_AUTO_IMG, image = r_images + i; i < r_numImages; i++, image++) {
         if (!image->name[0])
             continue;
         if (mask && !(mask & BIT(image->type)))
@@ -1595,7 +1595,7 @@ static image_t *alloc_image(void)
     image_t *image, *placeholder = NULL;
 
     // find a free image_t slot
-    for (i = 1, image = r_images + 1; i < r_numImages; i++, image++) {
+    for (i = R_NUM_AUTO_IMG, image = r_images + i; i < r_numImages; i++, image++) {
         if (!image->name[0])
             return image;
         if (!image->upload_width && !image->upload_height && !placeholder)
@@ -2030,14 +2030,27 @@ fail:
 
 image_t *IMG_Find(const char *name, imagetype_t type, imageflags_t flags)
 {
+    char buffer[MAX_QPATH];
     image_t *image;
+    size_t len;
 
     Q_assert(name);
 
-    if ((image = find_or_load_image(name, strlen(name), type, flags)))
-        return image;
+    // path MUST never overflow
+    len = FS_NormalizePathBuffer(buffer, name, sizeof(buffer));
+    image = find_or_load_image(buffer, len, type, flags);
 
-    return R_NOTEXTURE;
+    // missing (or invalid) sky texture will use default sky
+    if (type == IT_SKY) {
+        if (!image)
+            return R_SKYTEXTURE;
+        if (~image->flags & flags & IF_CUBEMAP)
+            return R_SKYTEXTURE;
+    }
+
+    if (!image)
+        return R_NOTEXTURE;
+    return image;
 }
 
 /*
@@ -2125,7 +2138,7 @@ void IMG_FreeUnused(void)
     image_t *image;
     int i, count = 0;
 
-    for (i = 1, image = r_images + 1; i < r_numImages; i++, image++) {
+    for (i = R_NUM_AUTO_IMG, image = r_images + i; i < r_numImages; i++, image++) {
         if (!image->name[0])
             continue;        // free image_t slot
         if (image->registration_sequence == r_registration_sequence)
@@ -2152,7 +2165,7 @@ void IMG_FreeAll(void)
     image_t *image;
     int i, count = 0;
 
-    for (i = 1, image = r_images + 1; i < r_numImages; i++, image++) {
+    for (i = R_NUM_AUTO_IMG, image = r_images + i; i < r_numImages; i++, image++) {
         if (!image->name[0])
             continue;        // free image_t slot
         // free it
@@ -2169,7 +2182,7 @@ void IMG_FreeAll(void)
         List_Init(&r_imageHash[i]);
 
     // &r_images[0] == R_NOTEXTURE
-    r_numImages = 1;
+    r_numImages = R_NUM_AUTO_IMG;
 }
 
 /*
@@ -2260,12 +2273,12 @@ void IMG_Init(void)
         List_Init(&r_imageHash[i]);
 
     // &r_images[0] == R_NOTEXTURE
-    r_numImages = 1;
+    r_numImages = R_NUM_AUTO_IMG;
 }
 
 void IMG_Shutdown(void)
 {
     Cmd_Deregister(img_cmd);
-    memset(r_images, 0, sizeof(r_images[0]));   // clear R_NOTEXTURE
+    memset(r_images, 0, R_NUM_AUTO_IMG * sizeof(r_images[0]));   // clear R_NOTEXTURE
     r_numImages = 0;
 }

src/refresh/images.h
@@ -76,7 +76,10 @@ extern int      r_numImages;
 
 extern unsigned r_registration_sequence;
 
-#define R_NOTEXTURE &r_images[0]
+#define R_NUM_AUTO_IMG  3
+#define R_NOTEXTURE     (&r_images[0])
+#define R_SHELLTEXTURE  (&r_images[1])
+#define R_SKYTEXTURE    (&r_images[2])
 
 extern uint32_t d_8to24table[256];
 

src/refresh/main.c
@@ -311,11 +311,15 @@ void GL_RotationMatrix(GLfloat *matrix)
     matrix[15] = 1;
 }
 
-void GL_RotateForEntity(void)
+void GL_RotateForEntity(bool skies)
 {
     GLfloat matrix[16];
 
     GL_RotationMatrix(matrix);
+    if (skies) {
+        GL_MultMatrix(gls.u_block.msky[0], glr.skymatrix[0], matrix);
+        GL_MultMatrix(gls.u_block.msky[1], glr.skymatrix[1], matrix);
+    }
     GL_MultMatrix(glr.entmatrix, glr.viewmatrix, matrix);
     GL_ForceMatrix(glr.entmatrix);
 }

src/refresh/mesh.c
@@ -570,7 +570,7 @@ static const image_t *skin_for_mesh(image_t **skins, int num_skins)
     const entity_t *ent = glr.ent;
 
     if (ent->flags & RF_SHELL_MASK)
-        return &shell_texture;
+        return R_SHELLTEXTURE;
 
     if (ent->skin)
         return IMG_ForHandle(ent->skin);
@@ -880,7 +880,7 @@ void GL_DrawAliasModel(const model_t *model)
             tess_static_plain : tess_lerped_plain;
     }
 
-    GL_RotateForEntity();
+    GL_RotateForEntity(false);
 
     GL_BindArrays(dotshading ? VA_MESH_SHADE : VA_MESH_FLAT);
 

commit 13c2c9b60bb276584ca934a9f9def522501b3c36

Fix rendering error if classic sky texture is missing.
Don't attempt to use cubemap as classic sky. Regression since 47656f88.

inc/refresh/refresh.h
@@ -156,12 +156,12 @@ typedef enum {
     IF_OPAQUE           = BIT(8),   // known to be opaque
     IF_DEFAULT_FLARE    = BIT(9),   // default flare hack
     IF_CUBEMAP          = BIT(10),  // cubemap (or part of it)
+    IF_CLASSIC_SKY      = BIT(11),  // split in two halves
 
     // these flags only affect R_RegisterImage() behavior,
     // and are not stored in image
     IF_OPTIONAL         = BIT(16),  // don't warn if not found
     IF_KEEP_EXTENSION   = BIT(17),  // don't override extension
-    IF_CLASSIC_SKY      = BIT(18),  // split in two halves
 } imageflags_t;
 
 typedef enum {

src/refresh/surf.c
@@ -574,7 +574,7 @@ static glStateBits_t statebits_for_surface(const mface_t *surf)
     glStateBits_t statebits = GLS_DEFAULT;
 
     if (surf->drawflags & SURF_SKY) {
-        if (Q_stricmpn(surf->texinfo->name, CONST_STR_LEN("n64/env/sky")) == 0)
+        if (surf->texinfo->image->flags & IF_CLASSIC_SKY)
             return GLS_TEXTURE_REPLACE | GLS_CLASSIC_SKY;
         else
             return GLS_TEXTURE_REPLACE | GLS_DEFAULT_SKY;