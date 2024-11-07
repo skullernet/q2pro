745cd746c7b3206a3c91aecbd4199fa0a8f4cb58
Andrey Nazarov <skuller32@gmail.com>
2024-11-07 12:46:20 +0300

    Turn ‘PF_cprintf to a non-client’ error into warning.
    No reason for this to be an error when other similar conditions are
    warnings.

--- src/server/game.c
+++ src/server/game.c
@@ -252,7 +252,8 @@ static void PF_cprintf(edict_t *ent, int level, const char *fmt, ...)
 
     clientNum = NUM_FOR_EDICT(ent) - 1;
     if (clientNum < 0 || clientNum >= sv_maxclients->integer) {
-        Com_Error(ERR_DROP, "%s to a non-client %d", __func__, clientNum);
+        Com_WPrintf("%s to a non-client %d\n", __func__, clientNum);
+        return;
     }
 
     client = svs.client_pool + clientNum;
