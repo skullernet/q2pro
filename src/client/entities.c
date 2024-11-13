skullernet/q2pro

commit 9e99f370a5b264b33c933d3f104192ce2f9252e0

Interpolate BFG lightramp in extended mode.

src/client/entities.c
@@ -978,11 +978,14 @@ static void CL_AddPacketEntities(void)
         } else if (effects & EF_FLIES) {
             CL_FlyEffect(cent, ent.origin);
         } else if (effects & EF_BFG) {
+            static const uint16_t bfg_lightramp[6] = {300, 400, 600, 300, 150, 75};
             if (effects & EF_ANIM_ALLFAST) {
                 CL_BfgParticles(&ent);
                 i = 200;
+            } else if (cl.csr.extended) {
+                i = bfg_lightramp[Q_clip(ent.oldframe, 0, 5)] * ent.backlerp +
+                    bfg_lightramp[Q_clip(ent.frame,    0, 5)] * cl.lerpfrac;
             } else {
-                static const uint16_t bfg_lightramp[6] = {300, 400, 600, 300, 150, 75};
                 i = bfg_lightramp[Q_clip(s1->frame, 0, 5)];
             }
             V_AddLight(ent.origin, i, 0, 1, 0);

commit 28163e3bf741838433e0115ee91ccff67d799bd2

Fix BFG lightramp lerp with variable FPS.
Regression since 9e99f370.

src/client/entities.c
@@ -993,7 +993,7 @@ static void CL_AddPacketEntities(void)
                 i = 200;
             } else if (cl.csr.extended) {
                 i = bfg_lightramp[Q_clip(ent.oldframe, 0, 5)] * ent.backlerp +
-                    bfg_lightramp[Q_clip(ent.frame,    0, 5)] * cl.lerpfrac;
+                    bfg_lightramp[Q_clip(ent.frame,    0, 5)] * (1.0f - ent.backlerp);
             } else {
                 i = bfg_lightramp[Q_clip(s1->frame, 0, 5)];
             }