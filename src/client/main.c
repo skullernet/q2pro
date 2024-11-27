b3ba4886eb5ef4a589c582036122c1183c9637d4
Andrey Nazarov <skuller32@gmail.com>
2024-11-28 01:52:57 +0300

    Show surface flags for ‘cl_surface’ macro.

--- src/client/main.c
+++ src/client/main.c
@@ -2297,7 +2297,7 @@ static size_t CL_Surface_m(char *buffer, size_t size)
 
     VectorMA(cl.refdef.vieworg, 8192, cl.v_forward, end);
     CL_Trace(&trace, cl.refdef.vieworg, end, vec3_origin, vec3_origin, MASK_SOLID | MASK_WATER);
-    return Q_strlcpy(buffer, trace.surface->name, size);
+    return Q_snprintf(buffer, size, "%s %#x", trace.surface->name, trace.surface->flags);
 }
 
 /*
