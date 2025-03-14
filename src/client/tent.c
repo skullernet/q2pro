902b7de08f25cc55ccf281596a32b49e6179133e
Andrey Nazarov <skuller32@gmail.com>
2025-03-14 07:33:17 +0300

    Remove unused explosion type.

--- src/client/tent.c
+++ src/client/tent.c
@@ -336,7 +336,6 @@ typedef struct {
         ex_flash,
         ex_mflash,
         ex_poly,
-        ex_poly2,
         ex_light
     } type;
 
@@ -551,16 +550,6 @@ static void CL_AddExplosions(void)
                 else
                     ent->skinnum = 6;
             }
-            break;
-        case ex_poly2:
-            if (f >= ex->frames - 1) {
-                ex->type = ex_free;
-                break;
-            }
-
-            ent->alpha = (5.0f - (float)f) / 5.0f;
-            ent->skinnum = 0;
-            ent->flags |= RF_TRANSLUCENT;
             break;
         default:
             Q_assert(!"bad type");
